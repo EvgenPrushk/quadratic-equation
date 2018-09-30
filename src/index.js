ada7dc23b9f59897c7bb150b156f2c75c8082f3a
EvgenPrushk <ogmprushak@mail.ru>
2018-09-30 17:41:39 +0300

    4 Task rdy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var NumberArray = equation.split(' ');   
+     for (var i = 1; i <  NumberArray.length; i++) {
+       if ( NumberArray[i] == "-" ) {
+        NumberArray[i+1] = -NumberArray[i+1];
+       }  
+   
+     }; 
+           /*Можно было использовать  for(var i=0; i< equation.length; i++) {  equation[i] = parseInt(equation[i], 10); }  */
+     var ABCArray = NumberArray.map(Number).filter(Boolean);
+       
+     var discriminant = ABCArray[1] * ABCArray[1] - 4 * NumberArray[0] * ABCArray[2]; console.log(ABCArray[2]); 
+    
+  
+  var rootEquation1 = (-ABCArray[1] - Math.sqrt(discriminant))/(2 * ABCArray[0]);   
+  rootEquation1 = Math.round(rootEquation1); console.log(rootEquation1);
+  var rootEquation2 = (-ABCArray[1] + Math.sqrt(discriminant))/(2 * ABCArray[0]);    
+   rootEquation2 = Math.round(rootEquation2); console.log(rootEquation2); 
+   if (discriminant > 0) {
+ return (rootEquation1 < rootEquation2) ? [rootEquation1, rootEquation2] : [rootEquation2, rootEquation1]
+   } else if (discriminant == 0) {return [((-ABCArray[1]) + Math.sqrt(discriminant))/(2 * ABCArray[1]), null] }
+ else { return [null, null]}
+
 }
+   
+
